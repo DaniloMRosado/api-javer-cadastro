f812db87f667481ff922c757c0beadd7d9a15340
DaniloMRosado <rosadomdanilo@gmail.com>
2024-08-13 13:48:43 -0300

    campo CPF adicionado

--- src/main/java/br/com/consulting_delivery/javer_api_client/JaverApiClientApplication.java
+++ src/main/java/br/com/consulting_delivery/javer_api_client/JaverApiClientApplication.java
@@ -1,13 +1,8 @@
 package br.com.consulting_delivery.javer_api_client;
 
-import br.com.consulting_delivery.javer_api_client.dto.DadosCadastroCliente;
-import br.com.consulting_delivery.javer_api_client.feign.DataManagerClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
